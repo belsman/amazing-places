ff2c1b9fee6a973d64b395ac245398a6bd2da432
Bello Babakolo <fleetbeekay@gmail.com>
2025-03-15 06:39:27 +0100

    Take photo and preview photo with image-picker

---
+++ components/ImagePicker.tsx
@@ -0,0 +1,55 @@
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
+import * as imagePicker from 'expo-image-picker';
+import { useState } from 'react';
+import { Colors } from '../constants/colors';
+
+export function ImagePicker() {
+  const [imageUri, setImageUri] = useState('');
+
+  // TODO Add permisions for IOS, camera will not work for now,
+  // Saw how to do this in a doc with the // useEffect when mounted
+  // and when the platform is an ios
+
+  const takePhoto = async () => {
+    const result = await imagePicker.launchCameraAsync({
+      aspect: [16, 9],
+      quality: 0.5,
+      mediaTypes: ['images'],
+      allowsEditing: true,
+    });
+
+    console.log('result', result);
+
+    if (!result.canceled) {
+      setImageUri(result.assets[0].uri);
+    }
+  };
+
+  return (
+    <>
+      <View style={style.imagePickerContainer}>
+        {imageUri ? (
+          <Image style={style.previewedImage} source={{ uri: imageUri }} />
+        ) : (
+          <Text>No image taken yet.</Text>
+        )}
+      </View>
+      <Button onPress={takePhoto} title="Take a photo" />
+    </>
+  );
+}
+
+const style = StyleSheet.create({
+  imagePickerContainer: {
+    width: '100%',
+    height: 200,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Colors.primary100,
+    marginVertical: 8,
+  },
+  previewedImage: {
+    width: '100%',
+    height: '100%',
+  },
+});
--- components/PlaceForm.tsx
+++ components/PlaceForm.tsx
@@ -1,9 +1,49 @@
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import { Colors } from '../constants/colors';
+import { ImagePicker } from './ImagePicker';
 
 export function PlaceForm() {
+  const [placeName, setPlaceName] = useState('');
+
   return (
-    <View>
-      <Text>Place Form</Text>
-    </View>
+    <ScrollView style={style.form}>
+      <View>
+        <Text style={style.label}>Place Form</Text>
+        <TextInput
+          value={placeName}
+          onChangeText={(value) => setPlaceName(value)}
+          style={style.input}
+        />
+      </View>
+      <ImagePicker />
+    </ScrollView>
   );
 }
+
+const style = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 16,
+  },
+  label: {
+    fontWeight: 'bold',
+    color: Colors.primary500,
+    margin: 4,
+  },
+  input: {
+    backgroundColor: Colors.primary100,
+    borderWidth: 2,
+    borderBottomColor: Colors.primary700,
+    paddingVertical: 8,
+    paddingHorizontal: 4,
+    marginVertical: 4,
+  },
+});
