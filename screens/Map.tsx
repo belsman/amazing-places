dc469ae83c52d440a79f487bf66ee059f3b29a0a
Bello Babakolo <fleetbeekay@gmail.com>
2025-03-16 07:24:55 +0100

    Add MapView from react-native-maps

--- screens/Map.tsx
+++ screens/Map.tsx
@@ -1,9 +1,30 @@
-import { View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export function Map() {
   return (
-    <View>
-      <Text>Map</Text>
+    <View style={style.root}>
+      <MapView
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        style={style.map}
+      >
+        <Marker coordinate={{ latitude: 37.78825, longitude: -122.4324 }} />
+      </MapView>
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
